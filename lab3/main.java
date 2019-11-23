71a45d9a4a2f75d7b1b1a8abbe50053452ad194e
maximzec <33631973+maximzec@users.noreply.github.com>
2019-11-23 09:52:16 +0300

    add main

---
+++ lab3/main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+class Main {
+
+    static public void main(String args[]) {
+        EffectiveProductionSystem main = new EffectiveProductionSystem();
+        main.main();
+    }
+}
